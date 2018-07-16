800fba4e4633c9d8eb0a04969475c07671297bd9
himaniexcellence <himani@excellencetechnologies.in>
2018-07-16 14:52:47 +0530

    remove view file, data is inserting by routing

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\UserData;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -31,13 +31,17 @@ Route::get('/v1/info', function(){
     return redirect()->route('infopage', ['id' => 1, 'page' => 'login']);
 });
 
-Route::get('/user', function(){
-    return view('userdata');
+Route::get('/user/{firstname}/{lastname}/{email}/{password}', function($fname, $lname, $email, $pass){
+    $data = new UserData();
+    $data->firstname = $fname;
+    $data->lastname = $lname;
+    $data->email = $email;
+    $data->password = $pass;
+    $data->save();
+    return $data;
 });
 
-Route::resource('/userdata', 'UserController');
-
 Route::get('/user/{id}', function($id){
-    $users = DB::table('user_datas')->where('id', $id)->get();
-    return $users; 
+    $users = DB::table('login_users')->where('id', $id)->get();
+    return $users;
 });
