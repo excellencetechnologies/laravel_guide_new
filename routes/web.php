1e56c22e599aede9f621a00b02bb21883a3c677b
nitdevv <nitesh@excellencetechnologies.in>
2018-07-18 16:27:52 +0530

    code updated with controllers

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,5 @@
 <?php
-use App\UserData;
-use App\Profile;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Input;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -42,88 +39,19 @@ Route::get("/user", function(){
 });
 
 // Creating User
-Route::post("/user", function(){
-    $data = new UserData();
-    $data->firstname = Input::get('fname');
-    $data->lastname = Input::get('lname');
-    $data->email = Input::get('email');
-    $data->password = Input::get('pass');
-    $data->save();
-    return $data;
-});
+Route::post("/user", 'UserController@createUser');
 
 // Fetching User Details
-Route::get('/user/{id}', function($id){
-    $users = DB::table('login_users')->where('id', $id)->count();
-    if($users > 0) {
-        $user = UserData::find($id);
-    } else {
-        return "User doesn't exist.";
-    }
-    return $user;
-});
+Route::get('/user/{id}', 'UserController@showUser');
 
 // Creating User Profile
-Route::post('/user/{id}', function($id){
-    $users = DB::table('login_users')->where('id', $id)->count();
-    
-    if($users > 0){
-        $data = new Profile();
-        $data->user_id = $id;
-        $data->address = Input::get('address');
-        $data->postal_code = Input::get('postcode');
-        $data->phone = Input::get('phone');
-        $data->save();
-        return $data;
-        
-    } else {
-        return "User doesn't exist";
-    }
-});
+Route::post('/user/{id}', 'UserController@createUserProfile');
 
 // Updating User Profile
-Route::put('/profile/{id}', function($id){
-    $profile = Profile::find($id);  
-    $profile->address = Input::get('address');
-    $profile->postal_code = Input::get('postcode');
-    $profile->phone = Input::get('phone');
-    $profile->save();
-    return $profile;
-});
+Route::put('/profile/{id}', 'UserController@updateUserProfile');
 
 // Deleting User Profile
-Route::delete('/user/profile/{id}', function($id){
-    $profile = Profile::find($id)->delete();
-    return "Profile Deleted.";
-});
+Route::delete('/user/profile/{id}', 'UserController@deleteUserProfile');
 
 // Fetching User Profile
-Route::get('/user/{id}/full', function($id){
-    $user = DB::table('login_users')->where('id', $id)->count();
-
-    if($user > 0){
-        
-        $profile = DB::table('profiles')->where('user_id', $id)->count();
-        
-        if($profile > 0){
-            $user_profile = DB::table('login_users')
-                                ->join('profiles', 'login_users.id', '=', 'profiles.user_id')
-                                ->select(
-                                    'login_users.id', 
-                                    'login_users.firstname', 
-                                    'login_users.lastname', 
-                                    'login_users.email',
-                                    'profiles.address',
-                                    'profiles.postal_code',
-                                    'profiles.phone'
-                                )->where('login_users.id', $id)->get();
-        } else {
-            $user_profile = UserData::find($id);
-        }
-
-    } else {
-        return "User doesn't exist.";
-    }
-    
-    return $user_profile;
-});
+Route::get('/user/{id}/full', 'UserController@showUserProfile');
